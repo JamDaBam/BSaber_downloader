2c4b6e2cbc56a19d1fefaeb24120f68640a94d1d
SaberMMM <->
2019-07-28 19:58:31 +0200

    Initial commit project

---
+++ bsaber-scrapper/src/main/java/bsaber/tools/bsaber_scrapper/App.java
@@ -0,0 +1,13 @@
+package bsaber.tools.bsaber_scrapper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
