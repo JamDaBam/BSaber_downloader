e7363e7131ece737ffe725d41ab3fe11cca63059
SaberMMM <->
2019-08-03 12:44:58 +0200

    Change download and saveroutine

--- bsaber-scrapper/src/main/java/bsaber/tools/bsaber_scrapper/BSaberEntry.java
+++ bsaber-scrapper/src/main/java/bsaber/tools/bsaber_scrapper/BSaberEntry.java
@@ -2,35 +2,41 @@ package bsaber.tools.bsaber_scrapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Set;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 public class BSaberEntry {
 	private static final Logger cvLogger = LogManager.getLogger(BSaberEntry.class);
 
-	private static final String DOWNLOAD_PATH = "Z:\\BSaberSongs\\";
+//	private static final String DOWNLOAD_PATH = "Z:\\BSaberSongs\\";
 	private static final String ILLEGAL_CHARACTERS = "[^a-zA-Z0-9\\.\\-]";
 
 	private static int cvNewDownloads = 0;
 	private static int cvAlreadyDownloads = 0;
 	private static int cvDownloadedTotal = 0;
-	private static List<BSaberEntry> cvNewSongs = new ArrayList<>();
-	private static List<BSaberEntry> cvSongsWithErrors = new ArrayList<>();
+	private static Set<BSaberEntry> cvNewSongs = new HashSet<>();
+	private static Set<BSaberEntry> cvSongsWithErrors = new HashSet<>();
 
 	private String ivSongID;
 	private String ivName;
 	private String ivDownloadUrl;
+	private String ivPath;
 	private boolean ivDownloaded = false;
 
-	public BSaberEntry(String aSongID, String aName, String aDownloadUrl) {
+	public BSaberEntry(String aSongID, String aName, String aDownloadUrl, String aPath) {
 		ivSongID = aSongID;
 		ivName = aName;
 		ivDownloadUrl = aDownloadUrl;
+		ivPath = aPath;
 	}
 
 	public String getSongID() {
@@ -43,6 +49,10 @@ public class BSaberEntry {
 
 	public String getDownloadUrl() {
 		return ivDownloadUrl;
+	}
+
+	public String getPath() {
+		return ivPath;
 	}
 
 	public boolean isDownloaded() {
@@ -65,33 +75,33 @@ public class BSaberEntry {
 		return cvDownloadedTotal;
 	}
 
-	public static List<BSaberEntry> getNewSongs() {
+	public static Set<BSaberEntry> getNewSongs() {
 		return cvNewSongs;
 	}
 
-	public static List<BSaberEntry> getSongsWithErrors() {
+	public static Set<BSaberEntry> getSongsWithErrors() {
 		return cvSongsWithErrors;
 	}
 
 	public static void printNewSongs() {
 
 		if (!getNewSongs().isEmpty()) {
-			cvLogger.debug("NEW SONGS:");
+			cvLogger.debug("_________");
+			cvLogger.debug("NEW SONGS");
 			for (BSaberEntry bSaberEntry : getNewSongs()) {
 				cvLogger.debug(bSaberEntry.toString());
 			}
-			cvLogger.debug("");
 		}
 	}
 
 	public static void printSongsWithErrors() {
 
 		if (!getSongsWithErrors().isEmpty()) {
+			cvLogger.debug("_________________");
 			cvLogger.debug("SONGS WITH ERRORS");
 			for (BSaberEntry bSaberEntry : getSongsWithErrors()) {
 				cvLogger.debug(bSaberEntry.toString());
 			}
-			cvLogger.debug("");
 		}
 	}
 
@@ -101,11 +111,15 @@ public class BSaberEntry {
 	}
 
 	public boolean download() {
-		File downloadFile = new File(DOWNLOAD_PATH + getDownloadName());
+		File downloadFile = new File(getPath() + getDownloadName());
 		try {
 			if (!downloadFile.exists()) {
+				if (!downloadFile.getParentFile().exists()) {
+					downloadFile.getParentFile().mkdirs();
+				}
+
 				URL url = new URL(getDownloadUrl());
-				FileUtils.copyURLToFile(url, downloadFile);
+				saveUrl(downloadFile.toPath(), url, 30, 30);
 
 				cvNewDownloads++;
 				cvDownloadedTotal++;
@@ -127,6 +141,26 @@ public class BSaberEntry {
 		}
 
 		return isDownloaded();
+	}
+
+	private static void saveUrl(final Path aFile, final URL aUrl, int aSecsConnectTimeout, int aSecsReadTimeout)
+			throws MalformedURLException, IOException {
+		try (InputStream in = streamFromUrl(aUrl, aSecsConnectTimeout, aSecsReadTimeout)) {
+			Files.copy(in, aFile);
+		}
+	}
+
+	private static InputStream streamFromUrl(URL aUrl, int aSecsConnectTimeout, int aSecsReadTimeout)
+			throws IOException {
+		URLConnection conn = aUrl.openConnection();
+		conn.setRequestProperty("User-Agent",
+				"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+
+		if (aSecsConnectTimeout > 0)
+			conn.setConnectTimeout(aSecsConnectTimeout * 1000);
+		if (aSecsReadTimeout > 0)
+			conn.setReadTimeout(aSecsReadTimeout * 1000);
+		return conn.getInputStream();
 	}
 
 	@Override
